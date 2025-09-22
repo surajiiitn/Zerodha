0e1e0f137da12ef784ef37e06ce2cdbc6d6ad9f0
surajshewale-hub <surajshewale654@gmail.com>
2025-09-22 20:23:18 +0530

    updated hero and award section

--- src/landing_page/home/Awards.js
+++ src/landing_page/home/Awards.js
@@ -3,7 +3,47 @@ import React from 'react';
 function Awards() {
     return (
         <>
-            <h1>Awards</h1>
+            <div className='container p-5 mb-5'>
+                <div className='row'>
+                    <div className='col-6'>
+                        <img src='media/images/largestBroker.svg'/>
+                    </div>
+                    <div className='col-6'>
+                        <h1>Largest stock Broker in India</h1>
+                        <p>2+ million Zerodha clients contribute to over 15% of all retail order volumes in India daily by trading and investing in:</p>
+                        <div className='row'>
+                            <div className="col-6">
+                                <ul>
+                            <li>
+                                <p> Futures and Options</p>
+                            </li>
+                            <li>
+                                <p> Commodity derivatives</p>
+                            </li>
+                            <li>
+                                <p> Currency derivatives</p>
+                            </li>
+                        </ul>
+                            </div>
+                            <div className="col-6">
+                                <ul>
+                            <li>
+                                <p> Stocks & IPOs</p>
+                            </li>
+                            <li>
+                                <p> Direct mutual funds</p>
+                            </li>
+                            <li>
+                                <p> Bonds and Govt. security</p>
+                            </li>
+                        </ul>
+                            </div>
+                            <img src='media/images/pressLogos.png'/>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
         </>
       );
 }
--- src/landing_page/home/Hero.js
+++ src/landing_page/home/Hero.js
@@ -2,9 +2,14 @@ import React from 'react';
 
 function Hero() {
     return ( 
-        <>
-        <h1>Hero</h1>
-        </>
+        <div className='container p-5'>
+            <div className="row text-center">
+                <img src='media/images/homeHero.png' alt='Image' className='mb-5'></img>
+                <h1 className='mt-5'>Invest in everything</h1>
+                <p>Online platform to invest in stocks,derivatives,mutual funds, and more</p>
+                <button className="p-2 btn btn-primary mb-5" style={{width:"15%",margin:"0 auto"}}>Signup Now</button>
+            </div>
+        </div>
      );
 }
 
