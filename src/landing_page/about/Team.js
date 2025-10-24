f5a03f0bec8ae88b55171e5de3f8af14dcd912a6
suraj kiran shewale <surajkiranshewale@surajs-MacBook-Air.local>
2025-10-24 15:59:47 +0530

    About page completed

--- src/landing_page/about/Team.js
+++ src/landing_page/about/Team.js
@@ -1,9 +1,27 @@
 import React from 'react';
 
 function Team() {
-    return ( 
-        <h1>Team</h1>
-     );
+    return (
+        <div className="container">
+            <div className="row p-3 mt-5 border-top">
+                <h1 className='fs-2 text-center'>People</h1>
+            </div>
+            <div className="row  text-muted text-center" style={{ lineHeight: "1.8", fontSize: "1em" }}>
+                <div className="col-6 p-3">
+                    <img src="media/images/nithinkamath.jpg" alt="ime" style={{ borderRadius: "100%", width: "40%" }} />
+                    <h4 className='mt-5'>Nithin  Kamath</h4>
+                    <h6>Founder , CEO</h6>
+                </div>
+                <div className="col-6 p-3">
+                    <p style={{textAlign:"start"}}>Nithin bootstrapped and founded Zerodha in 2010 to overcome the hurdles he faced during his decade long stint as a trader.
+                    Today, Zerodha has changed the landscape of the Indian broking industry. </p>
+                    <p style={{textAlign:"start"}}>He is a member of the SEBI Secondary Market Advisory Committee (SMAC) and the Market Data Advisory Committee (MDAC).</p>
+                    <p style={{textAlign:"start"}}>Playing basketball is his zen. </p>
+                    <p style={{textAlign:"start"}}>Connect on <a href="/">Homepage</a> / <a href="/">TradingQnA </a>/ <a href="/">Twitter</a></p>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Team;
